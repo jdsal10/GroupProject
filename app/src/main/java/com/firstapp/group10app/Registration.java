ae3dbb6232568992d4b1bcf8ddda2fd65f863fda
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-10-25 17:45:03 +0100

    Designed the skeleton of Registration p2 layout

--- app/src/main/java/com/firstapp/group10app/Registration.java
+++ app/src/main/java/com/firstapp/group10app/Registration.java
@@ -3,12 +3,25 @@ package com.firstapp.group10app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Registration extends AppCompatActivity {
+
+    RadioGroup radioGroup_MaleFemale;
+    RadioButton radioButton_MaleFemale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        radioGroup_MaleFemale = findViewById(R.id.radioGroup);
+    }
+
+    public void checkButton(View v) {
+        int radioId = radioGroup_MaleFemale.getCheckedRadioButtonId();
+        radioButton_MaleFemale = findViewById(radioId);
     }
 }
