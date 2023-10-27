2d2e0103e8a5a521349447d07cbdb884a69a0e31
rangerunnown <teatherethan@gmail.com>
2023-10-27 14:57:25 +0100

    Added basic functionality for transitions
    Will improve on the code formatting later, but basic ability to move to different page is complete.

--- app/src/main/java/com/firstapp/group10app/MainActivity.java
+++ app/src/main/java/com/firstapp/group10app/MainActivity.java
@@ -1,14 +1,51 @@
 package com.firstapp.group10app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.*;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button goToRegisterButton;
+    private Button goToLoginButton;
+
+    private TextView skipText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToRegisterButton = findViewById(R.id.goToRegister);
+        goToRegisterButton.setOnClickListener(this);
+
+        goToLoginButton = findViewById(R.id.goToLogin);
+        goToLoginButton.setOnClickListener(this);
+
+        skipText = findViewById(R.id.skipToHome);
+        skipText.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.goToRegister) {
+            Intent welcomeToRegister = new Intent(MainActivity.this, Registration.class);
+            startActivity(welcomeToRegister);
+        }
+        else if (id == R.id.goToLogin) {
+            Intent welcomeToLogin = new Intent(MainActivity.this, Login.class);
+            startActivity(welcomeToLogin);
+        }
+        else if (id == R.id.skipToHome) {
+            Intent skipToHome = new Intent(MainActivity.this, Home.class);
+            //Add form of identifier for the rest of the code to fucntion.
+            startActivity(skipToHome);
+        }
     }
 }
