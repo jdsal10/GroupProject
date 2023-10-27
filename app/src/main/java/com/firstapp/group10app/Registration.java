0178419c6b06ebc58d959dc0bd00543bf2ba374b
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-10-27 10:48:48 +0100

    Added a dropdown list for the reasons question
    Quickly added some stuff before out second group meeting for the week, so that I can show something more to the boys.

--- app/src/main/java/com/firstapp/group10app/Registration.java
+++ app/src/main/java/com/firstapp/group10app/Registration.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class Registration extends AppCompatActivity {
 
@@ -17,11 +19,28 @@ public class Registration extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        setSpinner(null);
+
         radioGroup_MaleFemale = findViewById(R.id.sexButtons);
     }
 
     public void checkButton(View v) {
         int radioId = radioGroup_MaleFemale.getCheckedRadioButtonId();
         radioButton_MaleFemale = findViewById(radioId);
+    }
+
+    public void setSpinner(View v) {
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.reasonsDropdown);
+
+        //create a list of items for the spinner.
+        String[] items = new String[]{"1", "2", "three"};
+
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
     }
 }
