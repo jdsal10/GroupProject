46e33a81c5e94af0fc4dcf85718126a0f7f7ba2a
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-10-28 18:55:22 +0100

    Big commit - Back and Continue buttons now work
    Wrote the logic and coded the functionality for the back and continue buttons in the Registration page :)

--- app/src/main/java/com/firstapp/group10app/Registration.java
+++ app/src/main/java/com/firstapp/group10app/Registration.java
@@ -2,26 +2,77 @@ package com.firstapp.group10app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
-public class Registration extends AppCompatActivity {
+public class Registration extends AppCompatActivity implements View.OnClickListener {
 
-    RadioGroup radioGroup_MaleFemale;
-    RadioButton radioButton_MaleFemale;
+    private RadioGroup radioGroup_MaleFemale;
+    private RadioButton radioButton_MaleFemale;
+    private LinearLayout page1, page2, page3;
+    private int activePage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        // Get the pages from the xml
+        page1 = findViewById(R.id.p1);
+        page2 = findViewById(R.id.p2);
+        page3 = findViewById(R.id.p3);
+
+        // Set the first page to be visible
+        page1.setVisibility(View.VISIBLE);
+        activePage = 1;
+
+        // Get the buttons from the xml
+        Button backButton = findViewById(R.id.buttonBack);
+        backButton.setOnClickListener(this);
+
+        Button nextButton = findViewById(R.id.buttonCont);
+        nextButton.setOnClickListener(this);
+
         setSpinner(null);
 
         radioGroup_MaleFemale = findViewById(R.id.sexButtons);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.buttonCont) {
+            if (activePage == 1) {
+                page1.setVisibility(View.GONE);
+                page2.setVisibility(View.VISIBLE);
+                activePage = 2;
+            } else if (activePage == 2) {
+                page2.setVisibility(View.GONE);
+                page3.setVisibility(View.VISIBLE);
+                activePage = 3;
+            } else if (activePage == 3) {
+                // TODO: Add code to send data to the server
+            }
+        } else if (id == R.id.buttonBack) {
+            if (activePage == 1) {
+                startActivity(new Intent(Registration.this, MainActivity.class));
+            } else if (activePage == 2) {
+                page2.setVisibility(View.GONE);
+                page1.setVisibility(View.VISIBLE);
+                activePage = 1;
+            } else if (activePage == 3) {
+                page3.setVisibility(View.GONE);
+                page2.setVisibility(View.VISIBLE);
+                activePage = 2;
+            }
+        }
     }
 
     public void checkButton(View v) {
