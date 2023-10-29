7a55fc27ebb2bc6c4c3a97cd51f1a893147090f9
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-10-29 14:04:23 +0000

    Reformatted code

--- app/src/main/java/com/firstapp/group10app/Registration.java
+++ app/src/main/java/com/firstapp/group10app/Registration.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -18,6 +19,7 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
     private RadioButton radioButton_MaleFemale;
     private LinearLayout page1, page2, page3;
     private int activePage;
+    private EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +31,21 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
         page2 = findViewById(R.id.p2);
         page3 = findViewById(R.id.p3);
 
-        // Set the first page to be visible
+        // Set page1 to be visible
         page1.setVisibility(View.VISIBLE);
         activePage = 1;
 
-        // Get the buttons from the xml
+        // Set the onClickListeners of the buttons
         Button backButton = findViewById(R.id.buttonBack);
         backButton.setOnClickListener(this);
 
         Button nextButton = findViewById(R.id.buttonCont);
         nextButton.setOnClickListener(this);
 
+        // Set the onTextChangedListener of the email field
+        email = findViewById(R.id.emailTextBox);
+
+        // Set the dropdown for the reasons for joining
         setSpinner(null);
 
         radioGroup_MaleFemale = findViewById(R.id.sexButtons);
@@ -48,30 +54,35 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        if (id == R.id.buttonCont) {
-            if (activePage == 1) {
-                page1.setVisibility(View.GONE);
-                page2.setVisibility(View.VISIBLE);
-                activePage = 2;
-            } else if (activePage == 2) {
-                page2.setVisibility(View.GONE);
-                page3.setVisibility(View.VISIBLE);
-                activePage = 3;
-            } else if (activePage == 3) {
-                // TODO: Add code to send data to the server
-            }
-        } else if (id == R.id.buttonBack) {
-            if (activePage == 1) {
-                startActivity(new Intent(Registration.this, MainActivity.class));
-            } else if (activePage == 2) {
-                page2.setVisibility(View.GONE);
-                page1.setVisibility(View.VISIBLE);
-                activePage = 1;
-            } else if (activePage == 3) {
-                page3.setVisibility(View.GONE);
-                page2.setVisibility(View.VISIBLE);
-                activePage = 2;
-            }
+        if (id == R.id.buttonCont) continuePressed();
+        else if (id == R.id.buttonBack) backPressed();
+    }
+
+    public void continuePressed() {
+        if (activePage == 1) {
+            page1.setVisibility(View.GONE);
+            page2.setVisibility(View.VISIBLE);
+            activePage = 2;
+        } else if (activePage == 2) {
+            page2.setVisibility(View.GONE);
+            page3.setVisibility(View.VISIBLE);
+            activePage = 3;
+        } else if (activePage == 3) {
+
+        }
+    }
+
+    public void backPressed() {
+        if (activePage == 1) {
+            startActivity(new Intent(Registration.this, MainActivity.class));
+        } else if (activePage == 2) {
+            page2.setVisibility(View.GONE);
+            page1.setVisibility(View.VISIBLE);
+            activePage = 1;
+        } else if (activePage == 3) {
+            page3.setVisibility(View.GONE);
+            page2.setVisibility(View.VISIBLE);
+            activePage = 2;
         }
     }
 
@@ -85,11 +96,10 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
         Spinner dropdown = findViewById(R.id.reasonsDropdown);
 
         //create a list of items for the spinner.
-        String[] items = new String[]{"1", "2", "three"};
+        String[] reasons = {"I want to lose weight", "I want to gain weight", "I want to maintain my weight"};
 
         //create an adapter to describe how the items are displayed, adapters are used in several places in android.
-        //There are multiple variations of this, but this is the basic variant.
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, reasons);
 
         //set the spinners adapter to the previously created one.
         dropdown.setAdapter(adapter);
