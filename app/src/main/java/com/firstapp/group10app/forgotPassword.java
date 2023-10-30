3c3de9ccd362dd3584f7c80bb648da24a62e8556
Misha Koroliovas <koroliov1985@gmail.com>
2023-10-30 03:34:57 +0000

    Created initial forgot password Page
    Page which sends an email to the user with further instructions.

---
+++ app/src/main/java/com/firstapp/group10app/forgotPassword.java
@@ -0,0 +1,13 @@
+package com.firstapp.group10app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class forgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
