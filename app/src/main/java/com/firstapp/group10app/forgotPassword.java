c4cac1edfa3756a4b1f4b08db507f042ec9460bf
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2023-10-30 18:58:42 +0000

    Added code to buttons

--- app/src/main/java/com/firstapp/group10app/forgotPassword.java
+++ app/src/main/java/com/firstapp/group10app/forgotPassword.java
@@ -1,13 +1,39 @@
 package com.firstapp.group10app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class forgotPassword extends AppCompatActivity {
+import android.widget.*;
+
+import android.view.*;
+
+public class forgotPassword extends AppCompatActivity implements View.OnClickListener {
+
+    private Button sendEmail;
+    private EditText emailToSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        emailToSend = findViewById(R.id.editTextTextEmailAddress);
+        emailToSend.setOnClickListener(this);
+
+        sendEmail = findViewById(R.id.sendEmail);
+        sendEmail.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.sendEmail) {
+            if (emailToSend.getText().toString().equals("")) {
+                //Send the email
+            }
+        }
     }
 }
