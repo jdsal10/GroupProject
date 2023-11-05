f0653960d2907ec4be5033e2357e0250f13ff8f2
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-11-05 22:23:35 +0000

    Debugging the chatGPT functionallity code

--- app/src/main/java/com/firstapp/group10app/chatGPT_API.java
+++ app/src/main/java/com/firstapp/group10app/chatGPT_API.java
@@ -1,52 +1,37 @@
 package com.firstapp.group10app;
 
-import java.io.*;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class chatGPT_API {
-    public static String chatGPT(String prompt) {
-        String url = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.7B";
-        String apiKey = "YOUR API_KEY";
+    public static String chatGPT(String prompt) throws Exception {
+        String url = "https://api.openai.com/v1/completions";
+        String apiKey = "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";
         String model = "gpt-3.5-turbo";
 
-        try {
-            URL obj = new URL(url);
-            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Authorization", "Bearer " + apiKey);
-            connection.setRequestProperty("Content-Type", "application/json");
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Authorization", "Bearer " + apiKey);
 
-            // The request body
-            String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
-            connection.setDoOutput(true);
-            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
-            writer.write(body);
-            writer.flush();
-            writer.close();
+        // The request body
+        JSONObject data = new JSONObject();
+        data.put("model", model);
+        data.put("prompt", prompt);
+        data.put("max_tokens", 150);
+        data.put("temperature", 1.0);
 
-            // Get the response
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-            StringBuffer response = new StringBuffer();
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-            reader.close();
+        connection.setDoOutput(true);
+        connection.getOutputStream().write(data.toString().getBytes());
 
-            // Return the response
-            return  extractMessageFromJSONResponse(response.toString());
+        String output = new BufferedReader(new InputStreamReader(connection.getInputStream())).lines()
+                .reduce((a, b) -> a + b).get();
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static String extractMessageFromJSONResponse(String response) {
-        int start = response.indexOf("content")+ 11;
-
-        int end = response.indexOf("\"", start);
-
-        return response.substring(start, end);
+        return new JSONObject(output).getJSONArray("choices").getJSONObject(0).getString("text");
     }
 }
