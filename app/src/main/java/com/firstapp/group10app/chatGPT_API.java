223c44ac8aeae98b657fa27cc8de0037e424150d
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-11-05 21:57:39 +0000

    Create chatGPT_API.java
    Created the chatGPT_API.java file to connect to the API with the API KEY from the new OpenAI account.

---
+++ app/src/main/java/com/firstapp/group10app/chatGPT_API.java
@@ -0,0 +1,52 @@
+package com.firstapp.group10app;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class chatGPT_API {
+    public static String chatGPT(String prompt) {
+        String url = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.7B";
+        String apiKey = "YOUR API_KEY";
+        String model = "gpt-3.5-turbo";
+
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization", "Bearer " + apiKey);
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            // The request body
+            String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
+            connection.setDoOutput(true);
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(body);
+            writer.flush();
+            writer.close();
+
+            // Get the response
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            // Return the response
+            return  extractMessageFromJSONResponse(response.toString());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String extractMessageFromJSONResponse(String response) {
+        int start = response.indexOf("content")+ 11;
+
+        int end = response.indexOf("\"", start);
+
+        return response.substring(start, end);
+    }
+}
