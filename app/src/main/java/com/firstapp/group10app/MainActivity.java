00413b9580f2d514a98e2bb1f39299553f712c9f
juan <juandasa74@gmail.com>
2023-11-06 22:41:19 +0000

    Revert "Added some SQL code"
    This reverts commit b728c7865f05e7e182ad0e158cc369a0f66a5b2f.

--- app/src/main/java/com/firstapp/group10app/MainActivity.java
+++ app/src/main/java/com/firstapp/group10app/MainActivity.java
@@ -8,17 +8,7 @@ import android.widget.Button;
 import android.content.Intent;
 import android.widget.TextView;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    public static final String DATABASE_NAME = "healthdata";
-    public static final String url = "jdbc:mysql://healthdata.cgzabjirm4kt.eu-west-2.rds.amazonaws.com:3306/" + DATABASE_NAME;
-    public static final String username = "healthadmin", password = "Comp6000health";
-//    public static final String
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,25 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         TextView skipText = findViewById(R.id.skipToHome);
         skipText.setOnClickListener(this);
-        System.out.println("Attempting");
-        new Thread(() -> {
-            try {
-                //Seems the code below is the issue, no print statements occur.
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection connection = DriverManager.getConnection(url, username, password);
-                System.out.println("Connected");
-                Statement statement = connection.createStatement();
-                System.out.println("Creating");
-        statement.execute("CREATE TABLE test");
-        connection.close();
-        System.out.println("SUCCESS");
-    } catch (Exception e) {
-        throw new RuntimeException(e);
     }
-    }).start();
-    }
-
-
 
     @Override
     public void onClick(View v) {
