db6459bc41ccebd7e4ba694e622a4c645310a433
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-11-06 18:21:04 +0000

    chatGPT.java documentation comments

--- app/src/main/java/com/firstapp/group10app/chatGPT.java
+++ app/src/main/java/com/firstapp/group10app/chatGPT.java
@@ -9,6 +9,10 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class chatGPT extends AsyncTask<String, Void, String> {
+    private static final String apiKey = "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";
+    private static final String model = "gpt-3.5-turbo";
+    private static final String url = "https://api.openai.com/v1/chat/completions";
+
     @Override
     protected String doInBackground(String... strings) {
         try {
@@ -20,42 +24,36 @@ public class chatGPT extends AsyncTask<String, Void, String> {
     }
 
     public static String chatGPT(String input) throws Exception {
-        String apiKey = "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";
-        String model = "gpt-3.5-turbo";
-
-        String url = "https://api.openai.com/v1/chat/completions";
+        // Connect to the API
         HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
-
         con.setRequestMethod("POST");
         con.setRequestProperty("Content-Type", "application/json");
         con.setRequestProperty("Authorization", "Bearer " + apiKey);
 
-        // The request body
-//        JSONObject data = new JSONObject();
-//        data.put("model", model);
-//        data.put("messages", input);
-//        data.put("max_tokens", 150);
-////        data.put("temperature", 1.0);
+        // Set the request body
         String prompt = "[{\"role\": \"user\", \"content\": \"" + input + "\"}]";
         int maxTokens = 150;
         String body = "{\"model\": \"" + model + "\", \"messages\": " + prompt + ", \"max_tokens\": " + maxTokens + "}";
 
+        // Send the request
         con.setDoOutput(true);
         OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
         writer.write(body);
         writer.flush();
 
-        System.out.println(body);
+        System.out.println(body); // For debugging purposes (remove later)
 
+        // Get the response
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
         StringBuffer response = new StringBuffer();
         while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine); // For debugging purposes (remove later)
             response.append(inputLine);
         }
         in.close();
 
-        System.out.println(response);
-        return response.toString();
+        System.out.println(response); // For debugging purposes (remove later)
+        return response.toString(); // Return the response
     }
 }
