eaf03bf67ea5955cda22f42b64eb68974c897d70
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2023-11-07 19:48:51 +0000

    Moved creation function to seperate file

--- app/src/main/java/com/firstapp/group10app/Pages/MainActivity.java
+++ app/src/main/java/com/firstapp/group10app/Pages/MainActivity.java
@@ -10,13 +10,8 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.TextView;
-
-
+import com.firstapp.group10app.DB.DBConnection;
 import com.firstapp.group10app.R;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
@@ -37,42 +32,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         connect();
     }
 
+    public void connect() {
+        DBConnection d = new DBConnection();
+        d.executeStatement("DROP TABLE IF EXISTS testing");
+        d.executeStatement("CREATE TABLE testing (ID INT PRIMARY KEY, Name VARCHAR(255), Age INT)");
 
-    public Connection connect() {
-
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        Connection conn = null;
-        Log Log = null;
-        try {
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            @SuppressLint("AuthLeak") String connectionString = "jdbc:mysql://gateway01.eu-central-1.prod.aws.tidbcloud.com:4000/test?user=4JhVNGoguqAHant.root&password=RRdHMohyyNYd0Trx&sslMode=VERIFY_IDENTITY&enabledTLSProtocols=TLSv1.2,TLSv1.3";
-            conn = DriverManager.getConnection(connectionString);
-
-            Statement st = conn.createStatement();
-            String dropTableSQL = "DROP TABLE IF EXISTS TestTable";
-            st.execute(dropTableSQL);
-
-            System.out.println("Table dropped successfully.");
-
-            st.execute("CREATE TABLE TestTable (" +
-                    "    ID INT PRIMARY KEY," +
-                    "    Name VARCHAR(255)," +
-                    "    Age INT," +
-                    "    Email VARCHAR(255)" +
-                    ");");
-            System.out.println("Table created successfully.");
-            st.close();
-            conn.close();
-        } catch (Exception e) {
-            Log.e("Connection", "Error connecting to the database: " + e.getMessage());
-            throw new RuntimeException(e);
-        }
-        return conn;
     }
-
-
-
 
     @Override
     public void onClick(View v) {
@@ -85,7 +50,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         else if (id == R.id.skipToHome) {
             startActivity(new Intent(MainActivity.this, Home.class));
-            //Add form of identifier for the rest of the code to function.
         }
     }
 }
