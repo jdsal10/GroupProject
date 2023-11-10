1f82843baf405863aa2eb555ce61bd054f464fd8
rangerunnown <teatherethan@gmail.com>
2023-11-10 17:45:57 +0000

    Removed test code

--- app/src/main/java/com/firstapp/group10app/Pages/MainActivity.java
+++ app/src/main/java/com/firstapp/group10app/Pages/MainActivity.java
@@ -1,20 +1,13 @@
 package com.firstapp.group10app.Pages;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.os.StrictMode;
-import android.util.Log;
 import android.view.*;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.TextView;
-import com.firstapp.group10app.DB.DBConnection;
 import com.firstapp.group10app.R;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -32,29 +25,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         TextView skipText = findViewById(R.id.skipToHome);
         skipText.setOnClickListener(this);
-        System.out.println("Attempting");
-        try {
-            connect();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
-    public void connect() throws SQLException {
-//        try {
-//            DBConnection d = new DBConnection();
-//            d.executeStatement("INSERT INTO HealthData.USER_TABLE (Email, PreferredName, Password) VALUES ('Ethan', 'test@gmail.com', 'guess123');");
-//            ResultSet set = d.executeQuery("SELECT * FROM HealthData.USER_TABLE");
-//            while (set.next()) {
-//                System.out.println(set.getString(3));
-//            }
-//        }
-//        catch (Exception e) {
-//            throw new SQLException(e);
-//        }
-//
-//
-    }
+
 
     @Override
     public void onClick(View v) {
