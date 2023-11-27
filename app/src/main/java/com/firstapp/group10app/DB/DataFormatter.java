03443fd13bc82b1a3d1a4889cb3f2f7ab62ab749
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-11-27 18:10:04 +0000

    Create DataFormatter.java

---
+++ app/src/main/java/com/firstapp/group10app/DB/DataFormatter.java
@@ -0,0 +1,15 @@
+package com.firstapp.group10app.DB;
+
+import java.util.ArrayList;
+
+/**
+ * DB.DataFormatter contains methods to format the data before passing it to the database.
+ */
+public class DataFormatter {
+    public static String[] formatUserDetails(String[] userDetails) {
+        ArrayList<String> formattedDetails = new ArrayList<>();
+
+
+        return formattedDetails.toArray(new String[0]);
+    }
+}
