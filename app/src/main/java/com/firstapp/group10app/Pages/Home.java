d26d30c528209ba6c05420b3a855dc75b9dcf21e
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2023-11-27 22:57:32 +0000

    Updated home
    Added a tempory button to take the user to the settings page

--- app/src/main/java/com/firstapp/group10app/Pages/Home.java
+++ app/src/main/java/com/firstapp/group10app/Pages/Home.java
@@ -1,15 +1,30 @@
 package com.firstapp.group10app.Pages;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firstapp.group10app.R;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button b = findViewById(R.id.gosettings);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.gosettings) {
+            Intent go = new Intent(Home.this, settings.class);
+            startActivity(go);
+        }
     }
 }
