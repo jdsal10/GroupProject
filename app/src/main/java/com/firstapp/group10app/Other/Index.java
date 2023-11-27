2aa246598ea660fe3bb8f0097a822843af132cf4
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-11-27 18:09:21 +0000

    Create Index.java

---
+++ app/src/main/java/com/firstapp/group10app/Other/Index.java
@@ -0,0 +1,13 @@
+package com.firstapp.group10app.Other;
+
+public class Index {
+    public static int EMAIL = 0;
+    public static int NAME = 1;
+    public static int PASSWORD = 2;
+    public static int DOB = 3;
+    public static int HEIGHT = 4;
+    public static int WEIGHT = 5;
+    public static int SEX = 6;
+    public static int CONDITIONS = 7;
+    public static int REASONS = 8;
+}
