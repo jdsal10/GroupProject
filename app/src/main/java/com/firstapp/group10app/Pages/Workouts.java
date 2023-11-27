eb30ffd4090b7d320907a58dac2a811905e4d4b4
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-11-27 12:39:26 +0000

    Generated Workouts Java class and an xml layout file

---
+++ app/src/main/java/com/firstapp/group10app/Pages/Workouts.java
@@ -0,0 +1,18 @@
+package com.firstapp.group10app.Pages;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firstapp.group10app.R;
+
+
+public class Workouts extends AppCompatActivity {
+    private boolean AI; // true if in AI mode, false if in manual mode
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workouts);
+    }
+}
