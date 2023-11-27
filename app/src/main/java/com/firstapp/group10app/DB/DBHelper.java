0582c383d5a97357a778d813afdafc28a9c80893
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-11-27 18:21:31 +0000

    Expanded DataFormatter.java (+ minor adjustments to DBHelper)

--- app/src/main/java/com/firstapp/group10app/DB/DBHelper.java
+++ app/src/main/java/com/firstapp/group10app/DB/DBHelper.java
@@ -1,8 +1,17 @@
 package com.firstapp.group10app.DB;
 
+import java.sql.ResultSet;
+
 public class DBHelper {
-    public void insertUser(String[] userDetails) {
+    public static void insertUser(String[] userDetails) {
         try {
+            // Check that the user details are valid
+            DataChecker.checkUserDetails(userDetails);
+
+            // Format the user details
+            // TODO: Finish DataFormatter
+            userDetails = DataFormatter.formatUserDetails(userDetails);
+
             // Create and SQL query
             StringBuilder sql = new StringBuilder();
             sql.append("INSERT INTO HealthData.Users (Email, PreferredName, Password, DOB, Weight, Height, Sex, HealthCondition, ReasonForDownloading) VALUES (");
@@ -17,6 +26,22 @@ public class DBHelper {
             // Execute the SQL query
             DBConnection db = new DBConnection();
             db.executeStatement(sql.toString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ResultSet getUser(String email) {
+        try {
+            // Create and SQL query
+            StringBuilder sql = new StringBuilder();
+            sql.append("SELECT * FROM HealthData.Users WHERE Email = '");
+            sql.append(email);
+            sql.append("';");
+
+            // Execute the SQL query
+            DBConnection db = new DBConnection();
+            return db.executeQuery(sql.toString());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- app/src/main/java/com/firstapp/group10app/DB/DataFormatter.java
+++ app/src/main/java/com/firstapp/group10app/DB/DataFormatter.java
@@ -1,5 +1,7 @@
 package com.firstapp.group10app.DB;
 
+import com.firstapp.group10app.Other.Index;
+
 import java.util.ArrayList;
 
 /**
@@ -9,7 +11,100 @@ public class DataFormatter {
     public static String[] formatUserDetails(String[] userDetails) {
         ArrayList<String> formattedDetails = new ArrayList<>();
 
+        formattedDetails.add(userDetails[Index.EMAIL]);
+        formattedDetails.add(formatName(userDetails[Index.NAME]));
+        formattedDetails.add(userDetails[Index.PASSWORD]);
+
+        // TODO: Implement the following methods
+        formattedDetails.add(formatDOB(userDetails[Index.DOB]));
+        formattedDetails.add(userDetails[Index.HEIGHT]);
+        formattedDetails.add(userDetails[Index.WEIGHT]);
+        formattedDetails.add(userDetails[Index.SEX]);
+        formattedDetails.add(userDetails[Index.CONDITIONS]);
+        formattedDetails.add(userDetails[Index.REASONS]);
 
         return formattedDetails.toArray(new String[0]);
+    }
+
+    /**
+     * Formats jOhn dOe -> John Doe
+     * Formats "" -> null
+     */
+    public static String formatName(String name) {
+        if (name.equals("")) {
+            return null;
+        }
+
+        // Capitalise the first letter of each word in the name
+        String[] nameArray = name.split(" ");
+        StringBuilder formattedName = new StringBuilder();
+        for (String word : nameArray) {
+            formattedName.append(word.substring(0, 1).toUpperCase());
+            formattedName.append(word.substring(1).toLowerCase());
+            formattedName.append(" ");
+        }
+
+        return formattedName.toString().trim();
+    }
+
+    /**
+     * Formats 01-01-2000 -> 2000-01-01
+     * Formats "" -> null
+     */
+    public static String formatDOB(String dob) {
+        if (dob.equals("")) {
+            return null;
+        }
+
+        // Format the date of birth
+        String[] dobArray = dob.split("-");
+        StringBuilder formattedDOB = new StringBuilder();
+        formattedDOB.append(dobArray[2]);
+        formattedDOB.append("-");
+        formattedDOB.append(dobArray[1]);
+        formattedDOB.append("-");
+        formattedDOB.append(dobArray[0]);
+
+        return formattedDOB.toString();
+    }
+
+    /**
+     * Converts 100 kg -> 100
+     * Converts 100 lbs -> 45.3592
+     * Converts "" -> null
+     */
+    public static String formatWeight(String weight, String units) {
+        if (weight.equals("")) {
+            return null;
+        }
+
+        // Convert the weight to kg
+        if (units.equals("kg")) {
+            return weight;
+        } else if (units.equals("lbs")) {
+            return String.valueOf(Double.parseDouble(weight) / 2.20462);
+        } else {
+            throw new RuntimeException("Invalid units!");
+        }
+    }
+
+    /**
+     * Converts 100 cm -> 100
+     * Converts 100 inch. -> 254
+     * Converts "" -> null
+     */
+    public static String formatHeight(String height, String units) {
+        if (height.equals("")) {
+            return null;
+        }
+
+        // Convert the height to cm
+        if (units.equals("cm")) {
+            return height;
+        } else if (units.equals("inch.")) {
+            return String.valueOf(Double.parseDouble(height) * 2.54);
+        } else {
+            throw new RuntimeException("Invalid units!");
+        }
     }
 }
