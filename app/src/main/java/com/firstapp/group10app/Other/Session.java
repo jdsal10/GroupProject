a076748750923c217a984e5686c7860f76138501
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-11-27 12:43:57 +0000

    Created a Session Java class to store and access important session info

---
+++ app/src/main/java/com/firstapp/group10app/Other/Session.java
@@ -0,0 +1,13 @@
+package com.firstapp.group10app.Other;
+
+public class Session {
+    private final String userID;
+
+    public Session(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+}
