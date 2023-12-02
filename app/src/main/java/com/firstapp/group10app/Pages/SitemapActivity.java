e787f5863a3e5cfbd37e5cdd8c28ef6114b8ba13
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-12-02 20:10:12 +0000

    Minor commit

--- app/src/main/java/com/firstapp/group10app/Pages/SitemapActivity.java
+++ app/src/main/java/com/firstapp/group10app/Pages/SitemapActivity.java
@@ -2,20 +2,25 @@ package com.firstapp.group10app.Pages;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firstapp.group10app.ChatGPT.ChatGPT_Client;
-import com.firstapp.group10app.ChatGPT.chatGPT_API;
 import com.firstapp.group10app.R;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SitemapActivity extends AppCompatActivity {
     private LinearLayout layout;
-    private boolean chatGPT_switch1 = false;
+    private final boolean chatGPT_switch1 = false;
 
 
     @Override
@@ -50,8 +55,7 @@ public class SitemapActivity extends AppCompatActivity {
 
         if (chatGPT_switch1 && chatGPT_switch2) {
             addText("ChatGPT");
-            addChatGPT("ChatGPT_Client");
-            addChatGPT("ChatGPT_API");
+            addChatGPT();
         } else {
             addText("ChatGPT functionality is disabled to prevent loss of credits.");
         }
@@ -60,12 +64,15 @@ public class SitemapActivity extends AppCompatActivity {
     private void addText(String text) {
         TextView textView = new TextView(this);
         textView.setText(text);
+        textView.setTextSize(20);
         layout.addView(textView);
     }
 
     private void addButton(String text, final Class<?> cls) {
         Button button = new Button(this);
         button.setText(text);
+        button.setTextSize(15);
+        button.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 0.5f));
         layout.addView(button);
 
         // Set the click listener to navigate to the corresponding page
@@ -77,34 +84,70 @@ public class SitemapActivity extends AppCompatActivity {
         });
     }
 
-    private void addChatGPT(String text) {
+    private void addChatGPT() {
         Button button = new Button(this);
-        button.setText(text);
+        button.setText("ChatGPT Client");
         layout.addView(button);
 
         // Set the click listener to navigate to the corresponding page
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    if (text.equals("ChatGPT_Client"))
-                        ChatGPT_Client.chatGPT("Hello");
-                    else if (text.equals("ChatGPT_API"))
-                        chatGPT_API.chatGPT("Hello");
-                    else
-                        throw new Exception("Invalid ChatGPT function");
-                } catch (Exception e) {
-                    e.printStackTrace();
+        button.setOnClickListener(v -> {
+            try {
+                ExecutorService executor = Executors.newSingleThreadExecutor();
+
+                StringBuilder output = new StringBuilder();
+
+                executor.execute(() -> {
+                    try {
+                        output.append(ChatGPT_Client.chatGPT("Hello"));
+//                                addPopUp(ChatGPT_Client.chatGPT("Hello"));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+                executor.shutdown();
+
+                while (!executor.isTerminated()) {
+                    // Wait for the executor to finish
                 }
+
+                addPopUp(output.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+        });
+    }
+
+    private void addPopUp(String text) {
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+        // set the text
+        TextView textView = findViewById(R.id.popup_text);
+        textView.setText(text);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        popupWindow.showAtLocation(layout, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener((v, event) -> {
+            popupWindow.dismiss();
+            return true;
         });
     }
 
     private void separateElements() {
         View view = new View(this);
-        view.setMinimumHeight(20);
+        view.setMinimumHeight(70);
         layout.addView(view);
     }
 
-    private boolean chatGPT_switch2 = false;
+    private final boolean chatGPT_switch2 = false;
 }
