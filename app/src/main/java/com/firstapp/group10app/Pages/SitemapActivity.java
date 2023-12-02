a0f17c890f3a7539a93ec22063841311ed443781
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-12-02 18:51:33 +0000

    Added text, dividers, and functionality buttons to the sitemap

--- app/src/main/java/com/firstapp/group10app/Pages/SitemapActivity.java
+++ app/src/main/java/com/firstapp/group10app/Pages/SitemapActivity.java
@@ -5,34 +5,65 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firstapp.group10app.ChatGPT.ChatGPT_Client;
+import com.firstapp.group10app.ChatGPT.chatGPT_API;
 import com.firstapp.group10app.R;
+
 public class SitemapActivity extends AppCompatActivity {
+    private LinearLayout layout;
+    private boolean chatGPT_switch1 = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sitemap);
 
-        LinearLayout layout = findViewById(R.id.sitemap_layout);
+        layout = findViewById(R.id.sitemap_layout);
 
-        // Add a button for each page in the app
+        addText("Sitemap");
+
+        separateElements();
+
         // Sprint 1 pages
-        addButton(layout, "MainActivity", MainActivity.class);
-        addButton(layout, "Registration Page", Registration.class);
-        addButton(layout, "Login Page", Login.class);
-        addButton(layout, "Forgot Password Page", ForgotPassword.class);
-        addButton(layout, "Forgot Password Check Page", forgotpasswordcheck.class);
-        addButton(layout, "Forgot Password Continued Page", ForgotPasswordContinued.class);
+        addText("Sprint 1 Pages");
+        addButton("MainActivity", MainActivity.class);
+        addButton("Registration Page", Registration.class);
+        addButton("Login Page", Login.class);
+        addButton("Forgot Password Page", ForgotPassword.class);
+        addButton("Forgot Password Check Page", forgotpasswordcheck.class);
+        addButton("Forgot Password Continued Page", ForgotPasswordContinued.class);
+
+        separateElements();
 
         // Sprint 2 pages
-        addButton(layout, "Home Page", Home.class);
-        addButton(layout, "Workouts Page", Workouts.class);
-        addButton(layout, "Settings Page", settings.class);
+        addText("Sprint 2 Pages");
+        addButton("Home Page", Home.class);
+        addButton("Workouts Page", Workouts.class);
+        addButton("Settings Page", settings.class);
+
+        separateElements();
+
+        if (chatGPT_switch1 && chatGPT_switch2) {
+            addText("ChatGPT");
+            addChatGPT("ChatGPT_Client");
+            addChatGPT("ChatGPT_API");
+        } else {
+            addText("ChatGPT functionality is disabled to prevent loss of credits.");
+        }
     }
 
-    private void addButton(LinearLayout layout, String text, final Class<?> cls) {
+    private void addText(String text) {
+        TextView textView = new TextView(this);
+        textView.setText(text);
+        layout.addView(textView);
+    }
+
+    private void addButton(String text, final Class<?> cls) {
         Button button = new Button(this);
         button.setText(text);
         layout.addView(button);
@@ -45,4 +76,35 @@ public class SitemapActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void addChatGPT(String text) {
+        Button button = new Button(this);
+        button.setText(text);
+        layout.addView(button);
+
+        // Set the click listener to navigate to the corresponding page
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (text.equals("ChatGPT_Client"))
+                        ChatGPT_Client.chatGPT("Hello");
+                    else if (text.equals("ChatGPT_API"))
+                        chatGPT_API.chatGPT("Hello");
+                    else
+                        throw new Exception("Invalid ChatGPT function");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void separateElements() {
+        View view = new View(this);
+        view.setMinimumHeight(20);
+        layout.addView(view);
+    }
+
+    private boolean chatGPT_switch2 = false;
 }
