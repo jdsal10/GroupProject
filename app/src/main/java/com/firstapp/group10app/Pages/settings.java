19ce9fe8c93f9d9ac29d63bedfbf1d313da2fc86
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2023-12-04 16:33:14 +0000

    Implement click functionality

--- app/src/main/java/com/firstapp/group10app/Pages/settings.java
+++ app/src/main/java/com/firstapp/group10app/Pages/settings.java
@@ -1,16 +1,61 @@
 package com.firstapp.group10app.Pages;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.firstapp.group10app.Fragments.settings_access;
 import com.firstapp.group10app.R;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class settings extends AppCompatActivity {
+public class settings extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener, View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.bottemNv, settings_access.class, null)
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected (MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.opensettings)
+        {
+            startActivity(new Intent(getApplicationContext(),settings.class));
+            return true;
+        }
+        else if(id == R.id.gostats) {
+            return true;
+            //Code for stats
+        }
+        else if(id == R.id.gohome) {
+            startActivity(new Intent(getApplicationContext(),Home.class));
+            return true;
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.goDataControl) {
+            //Go data
+        }
+        else if(id == R.id.goAccessibility) {
+            //Go access
+        }
+        else if(id == R.id.goAccount) {
+            //Go account
+        }
     }
 }
