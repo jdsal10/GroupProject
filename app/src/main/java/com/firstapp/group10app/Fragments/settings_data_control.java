ab06eacbbbe30fa0ebdce627a698d91d4c7bdb01
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2023-12-08 22:52:06 +0000

    Added basic functionality
    Gives it the ability to get data about the user

--- app/src/main/java/com/firstapp/group10app/Fragments/settings_data_control.java
+++ app/src/main/java/com/firstapp/group10app/Fragments/settings_data_control.java
@@ -7,60 +7,75 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.firstapp.group10app.DB.DBHelper;
+import com.firstapp.group10app.Other.*;
 import com.firstapp.group10app.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BlankFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class settings_data_control extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    EditText dobValue;
+    EditText weightValue;
+    EditText heightValue;
+    EditText sexValue;
+    EditText allergiesValue;
+    EditText reasonsValue;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static ArrayList<String> details = new ArrayList<>();
 
-    public settings_data_control() {super(R.layout.fragment_settings_data_control);
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static settings_data_control newInstance(String param1, String param2) {
-        settings_data_control fragment = new settings_data_control();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public settings_data_control() {
+        super(R.layout.fragment_settings_data_control);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    }
+
+    public void updateValues(ArrayList<String> info) {
+        dobValue.setText(info.get(0));
+        weightValue.setText(info.get(1));
+        heightValue.setText(info.get(2));
+        sexValue.setText(info.get(3));
+        allergiesValue.setText(info.get(4));
+        reasonsValue.setText(info.get(5));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings_data_control, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_settings_data_control, container, false);
+
+        dobValue = rootView.findViewById(R.id.DOBValue);
+        weightValue = rootView.findViewById(R.id.weightValue);
+        heightValue = rootView.findViewById(R.id.heightValue);
+        sexValue = rootView.findViewById(R.id.sexValue);
+        allergiesValue = rootView.findViewById(R.id.allerguesValue);
+        reasonsValue = rootView.findViewById(R.id.reasonValue);
+
+        String currentUser = Session.userEmail;
+        details = new ArrayList<>();
+
+        ResultSet data = DBHelper.getUser(currentUser);
+
+        try {
+            if (data.next()) { // Move the cursor to the first row
+                details.add(data.getString("DOB"));
+                details.add(data.getString("Weight"));
+                details.add(data.getString("Height"));
+                details.add(data.getString("Sex"));
+                details.add(data.getString("HealthCondition"));
+                details.add(data.getString("ReasonForDownloading"));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        updateValues(details);
+        return rootView;
     }
 }
