bac5017d6f663a361bd9a41d1ed5eaad4051c030
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2023-12-08 22:51:39 +0000

    Modified to allow for values to be updated

--- app/src/main/java/com/firstapp/group10app/Other/Session.java
+++ app/src/main/java/com/firstapp/group10app/Other/Session.java
@@ -4,7 +4,7 @@ package com.firstapp.group10app.Other;
  * The Session class stores valuable information about the user's session.
  */
 public class Session {
-    private final String userEmail;
+    public static String userEmail;
 
     public Session(String userEmail) {
         this.userEmail = userEmail;
