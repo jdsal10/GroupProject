4c9c855ab5a0a72bdc3608b5d041e68caea778c9
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2023-12-19 14:30:09 +0000

    Added logic for account deletion

--- app/src/main/java/com/firstapp/group10app/Fragments/settings_account.java
+++ app/src/main/java/com/firstapp/group10app/Fragments/settings_account.java
@@ -1,5 +1,6 @@
 package com.firstapp.group10app.Fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,76 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.firstapp.group10app.DB.DBHelper;
+import com.firstapp.group10app.Other.Session;
 import com.firstapp.group10app.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link settings_account#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class settings_account extends Fragment {
+import java.sql.SQLException;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1", ARG_PARAM2 = "param2";
+public class settings_account extends Fragment implements View.OnClickListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1, mParam2;
-
+    Button deleteAccount;
     public settings_account() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment settings_account.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static settings_account newInstance(String param1, String param2) {
-        settings_account fragment = new settings_account();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings_account, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_settings_account, container, false);
+
+        deleteAccount = rootView.findViewById(R.id.deleteAccountButton);
+        deleteAccount.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.deleteAccountButton) {
+        showConfirmation();
+        }
+    }
+
+    public void showConfirmation() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+        LayoutInflater inflater = getLayoutInflater();
+
+        View dialogView = inflater.inflate(R.layout.delete_account_confirm, null);
+        builder.setView(dialogView);
+        AlertDialog alertDialog = builder.create();
+
+        Button confirm = dialogView.findViewById(R.id.deleteConfirm);
+        EditText passwordDelete = dialogView.findViewById(R.id.accountDeletePassword);
+
+        confirm.setOnClickListener(v -> {
+            String password = passwordDelete.getText().toString();
+            DBHelper db = new DBHelper();
+            try {
+                if(db.checkUser(Session.userEmail, password)) {
+                    System.out.println("CONFIRM DELETION!");
+                    // Add logic for deletion below - requires integration to workouts.
+                }
+                else {
+                    passwordDelete.setError("Incorrect password used.");
+//                    alertDialog.dismiss();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        // Show the dialog
+        alertDialog.show();
     }
 }
