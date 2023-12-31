3908a7c9655d6b04165629bff90fa10a3675c56d
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2023-12-31 15:24:53 +0000

    Added function for sql to json BETA FUNCTION
    Need to improve function and adapt for other uses

--- app/src/main/java/com/firstapp/group10app/Pages/MainActivity.java
+++ app/src/main/java/com/firstapp/group10app/Pages/MainActivity.java
@@ -12,6 +12,9 @@ import com.firstapp.group10app.DB.DBConnection;
 import com.firstapp.group10app.Other.Session;
 import com.firstapp.group10app.R;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
@@ -31,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // TEMP CODE TO CHECK CONNECTION
         Session.dbStatus = DBConnection.testConnection();
+        test();
     }
 
     @Override
@@ -44,6 +48,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(new Intent(MainActivity.this, Home.class));
         } else if (id == R.id.goToSitemap) {
             startActivity(new Intent(MainActivity.this, SitemapActivity.class));
+        }
+    }
+
+    public void test() {
+        DBConnection d = new DBConnection();
+        ResultSet out = d.executeQuery("SELECT\n" +
+                "  JSON_ARRAYAGG(\n" +
+                "    JSON_OBJECT(\n" +
+                "      'WorkoutID', w.WorkoutID,\n" +
+                "      'WorkoutName', w.WorkoutName,\n" +
+                "      'WorkoutDuration', w.WorkoutDuration,\n" +
+                "      'TargetMuscleGroup', w.TargetMuscleGroup,\n" +
+                "      'Equipment', w.Equipment,\n" +
+                "      'Difficulty', w.Difficulty,\n" +
+                "      'Exercises', (\n" +
+                "        SELECT JSON_ARRAYAGG(\n" +
+                "          JSON_OBJECT(\n" +
+                "            'ExerciseID', e.ExerciseID,\n" +
+                "            'ExerciseName', e.ExerciseName,\n" +
+                "            'Description', e.Description,\n" +
+                "            'Illustration', e.Illustration,\n" +
+                "            'TargetMuscleGroup', e.TargetMuscleGroup,\n" +
+                "            'Equipment', e.Equipment,\n" +
+                "            'Difficulty', e.Difficulty\n" +
+                "          )\n" +
+                "        )\n" +
+                "        FROM HealthData.ExerciseWorkoutPairs ewp\n" +
+                "        JOIN HealthData.Exercises e ON ewp.ExerciseID = e.ExerciseID\n" +
+                "        WHERE ewp.WorkoutID = w.WorkoutID\n" +
+                "      )\n" +
+                "    )\n" +
+                "  ) AS Result\n" +
+                "FROM\n" +
+                "  HealthData.Workouts w;\n");
+
+        while (true) {
+            try {
+                if (!out.next()) break;
+                for (int i = 1; i <= out.getMetaData().getColumnCount(); i++) {
+                    System.out.print(out.getString(i) + "\t");
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println();
         }
     }
 }
