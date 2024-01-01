b6a5509448f23284a154d4e92f6dd24a470dc7ed
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2024-01-01 17:21:45 +0000

    Added click and navigation bar functionality

--- app/src/main/java/com/firstapp/group10app/createOrSearch.java
+++ app/src/main/java/com/firstapp/group10app/createOrSearch.java
@@ -2,13 +2,55 @@ package com.firstapp.group10app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class createOrSearch extends AppCompatActivity {
+import com.firstapp.group10app.Pages.Home;
+import com.firstapp.group10app.Pages.Settings;
+import com.firstapp.group10app.Pages.searchWorkout;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class createOrSearch extends AppCompatActivity implements View.OnClickListener, NavigationBarView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_or_search);
+
+        Button goToSearch = findViewById(R.id.goToSearch);
+        goToSearch.setOnClickListener(this);
+
+        Button goToCreate = findViewById(R.id.goToCreate);
+        goToCreate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.goToSearch) {
+            startActivity(new Intent(createOrSearch.this, searchWorkout.class));
+        } else if (id == R.id.goToCreate) {
+            // Update with correct file when created!
+//            startActivity(new Intent(createOrSearch.this, searchWorkout.class));
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.goSettings) {
+            startActivity(new Intent(getApplicationContext(), Settings.class));
+            return true;
+        } else if (id == R.id.goStats) {
+            return true;
+            //Code for stats
+        } else if (id == R.id.goHome) {
+            startActivity(new Intent(getApplicationContext(), Home.class));
+            return true;
+        }
+        return true;
     }
 }
