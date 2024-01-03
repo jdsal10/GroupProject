84aba64e8fcc8df2ec1727e9ce5a9faa104ec0f4
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2024-01-03 16:07:39 +0000

    Added functionality to view exercises
    Needs some tweaking mainly on the format but seems to work well

--- app/src/main/java/com/firstapp/group10app/Pages/searchWorkout.java
+++ app/src/main/java/com/firstapp/group10app/Pages/searchWorkout.java
@@ -2,9 +2,12 @@ package com.firstapp.group10app.Pages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -20,7 +23,7 @@ import org.json.JSONObject;
 import java.sql.SQLException;
 
 public class searchWorkout extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
-    private LinearLayout mainLayout;
+    private LinearLayout workoutLayout, exerciseLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +31,10 @@ public class searchWorkout extends AppCompatActivity implements NavigationBarVie
         setContentView(R.layout.activity_search_workout);
 
         ScrollView scrollView = findViewById(R.id.resultSearchWorkout);
-        mainLayout = new LinearLayout(this);
+        workoutLayout = new LinearLayout(this);
+        workoutLayout.setOrientation(LinearLayout.VERTICAL);
 
-        scrollView.addView(mainLayout);
+        scrollView.addView(workoutLayout);
 
         try {
             updateWorkouts();
@@ -40,7 +44,7 @@ public class searchWorkout extends AppCompatActivity implements NavigationBarVie
     }
 
     public void updateWorkouts() throws JSONException, SQLException {
-        String input = DBHelper.test();
+        String input = DBHelper.getAllWorkouts();
         System.out.println(input);
         JSONArray jsonArray = new JSONArray(input);
 
@@ -51,10 +55,83 @@ public class searchWorkout extends AppCompatActivity implements NavigationBarVie
     }
 
     public void addDetails(JSONObject details) {
-        LinearLayout box = (LinearLayout) getLayoutInflater().inflate(R.layout.workoutview, null);
-        TextView viewDetails = box.findViewById(R.id.workoutDetailsTextView);
-        viewDetails.setText(details.optString("WorkoutName", "") + " ++  " + details.optString("Exercises", ""));
-        mainLayout.addView(box);
+        LinearLayout box = (LinearLayout) getLayoutInflater().inflate(R.layout.activity_workout_view, null);
+
+        TextView nameView = box.findViewById(R.id.workoutNameView);
+        TextView durationView = box.findViewById(R.id.workoutDurationView);
+        TextView muscleView = box.findViewById(R.id.workoutMuscleView);
+        TextView equipmentView = box.findViewById(R.id.workoutEquipmentView);
+        TextView difficultyView = box.findViewById(R.id.workoutDifficultyView);
+
+        box.setId(details.optInt("WorkoutID"));
+
+        // Sets the textViews to the appropriate details.
+        nameView.setText("Workout Name: " + details.optString("WorkoutName", ""));
+        durationView.setText("Workout Duration: " + details.optString("WorkoutDuration", ""));
+        muscleView.setText("Target Muscle Group: " + details.optString("TargetMuscleGroup", ""));
+        equipmentView.setText("Equipment: " + details.optString("Equipment", ""));
+        difficultyView.setText("Difficulty: " + details.optString("Difficulty", ""));
+
+        String exerciseList = details.optString("Exercises");
+
+        // Adds to a linear layout.
+        workoutLayout.addView(box);
+
+        // For now, clicking on a workout shows the exercises - may make easier later.
+        box.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = getLayoutInflater();
+
+            View dialogView = inflater.inflate(R.layout.activity_exercise_popup, null);
+
+            builder.setView(dialogView);
+            AlertDialog alertDialog = builder.create();
+            ScrollView exerciseMainView = dialogView.findViewById(R.id.exerciseMainView);
+
+            // Creates a layout containing the exercise boxes.
+            exerciseLayout = new LinearLayout(this);
+            exerciseLayout.setOrientation(LinearLayout.VERTICAL);
+
+            JSONArray jsonArray = null;
+            try {
+                jsonArray = new JSONArray(exerciseList);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+
+            // For every exercise, we create a box containing the details.
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject workoutObject = null;
+
+                try {
+                    workoutObject = jsonArray.getJSONObject(i);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                LinearLayout exerciseBox = (LinearLayout) getLayoutInflater().inflate(R.layout.activity_exercise_view, null);
+
+                TextView exerciseNameView = exerciseBox.findViewById(R.id.exerciseNameView);
+                TextView exerciseDescriptionView = exerciseBox.findViewById(R.id.exerciseDescriptionView);
+                TextView exerciseIllustrationView = exerciseBox.findViewById(R.id.exerciseIllustrationView);
+                TextView exerciseTargetMuscleGroupView = exerciseBox.findViewById(R.id.exerciseTargetMuscleGroupView);
+                TextView exerciseEquipmentView = exerciseBox.findViewById(R.id.exerciseEquipmentView);
+                TextView exerciseDifficultyView = exerciseBox.findViewById(R.id.exerciseDifficultyView);
+
+                exerciseNameView.setText("Exercise Name: " + workoutObject.optString("ExerciseName", ""));
+                exerciseDescriptionView.setText("Exercise Description: " + workoutObject.optString("Description", ""));
+                exerciseIllustrationView.setText("Exercise Illustration: " + workoutObject.optString("Illustration", ""));
+                exerciseTargetMuscleGroupView.setText("Exercise Target Group: " + workoutObject.optString("TargetMuscleGroup", ""));
+                exerciseEquipmentView.setText("Exercise Equipment: " + workoutObject.optString("Equipment", ""));
+                exerciseDifficultyView.setText("Exercise Difficulty: " + workoutObject.optString("Difficulty", ""));
+
+                exerciseLayout.addView(exerciseBox);
+            }
+            // Adds the Linear layout containing all boxes to the scroll view.
+            exerciseMainView.addView(exerciseLayout);
+
+            alertDialog.show();
+        });
     }
 
     @Override
@@ -72,4 +149,5 @@ public class searchWorkout extends AppCompatActivity implements NavigationBarVie
         }
         return true;
     }
+
 }
