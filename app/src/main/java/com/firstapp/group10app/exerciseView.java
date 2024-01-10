a3c073fcc120cc948ec3c58c5c2d7e071f20f619
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2024-01-10 14:57:48 +0000

    Added new button
    New button will select a workout and take the user to "selected workout" screen - yet to be made

--- app/src/main/java/com/firstapp/group10app/exerciseView.java
+++ app/src/main/java/com/firstapp/group10app/exerciseView.java
@@ -2,13 +2,32 @@ package com.firstapp.group10app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class exerciseView extends AppCompatActivity {
+import com.firstapp.group10app.Pages.Home;
+
+public class exerciseView extends AppCompatActivity implements View.OnClickListener {
+
+    private Button selectWorkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_popup);
+        
+        selectWorkout = findViewById(R.id.selectWorkout);
+        selectWorkout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.selectWorkout) {
+            // Add workout to session info - potential idea
+            startActivity(new Intent(exerciseView.this, Home.class));
+        }
     }
 }
