6516f9636fd411d3655ffe8e2f8474fe5cb76a45
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2024-01-22 19:19:18 +0000

    Added basic logic for toggle

--- app/src/main/java/com/firstapp/group10app/Pages/workout_option.java
+++ app/src/main/java/com/firstapp/group10app/Pages/workout_option.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 
 import com.firstapp.group10app.R;
@@ -14,30 +15,40 @@ public class workout_option extends AppCompatActivity implements CompoundButton.
 
     public RadioButton AISelect;
     public RadioButton manualSelect;
+    public LinearLayout aiView, manualView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_option);
 
         // Initialize RadioButtons
-        AISelect = findViewById(R.id.AI);
-        manualSelect = findViewById(R.id.manual);
+        AISelect = findViewById(R.id.toggleAI);
+        manualSelect = findViewById(R.id.toggleManual);
 
         // Set OnCheckedChangeListener
         AISelect.setOnCheckedChangeListener(this);
         manualSelect.setOnCheckedChangeListener(this);
+
+        aiView = findViewById(R.id.aiView);
+        manualView = findViewById(R.id.manualView);
     }
 
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if(isChecked) {
-            if(buttonView.getId() == R.id.AI) {
+        if (isChecked) {
+            if (buttonView.getId() == R.id.toggleAI) {
                 System.out.println("AI SELECT");
+                manualView.setVisibility(View.GONE);
+                aiView.setVisibility(View.VISIBLE);
+
+            } else if (buttonView.getId() == R.id.toggleManual) {
+                System.out.println("MAN SELECT");
+                aiView.setVisibility(View.GONE);
+                manualView.setVisibility(View.VISIBLE);
             }
-            else if(buttonView.getId() == R.id.manual) {
-            System.out.println("MAN SELECT");
-        }}
+        }
     }
 
 
