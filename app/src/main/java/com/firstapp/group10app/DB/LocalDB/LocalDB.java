feea8953a2691af93a136d9e4954575c1872fb6c
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2024-01-27 12:58:27 +0000

    Update LocalDB.java
    I am following Google's guidelines: https://developer.android.com/training/data-storage/sqlite

--- app/src/main/java/com/firstapp/group10app/DB/LocalDB/LocalDB.java
+++ app/src/main/java/com/firstapp/group10app/DB/LocalDB/LocalDB.java
@@ -1,51 +1,212 @@
 package com.firstapp.group10app.DB.LocalDB;
 
+
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
-import androidx.annotation.Nullable;
+import com.firstapp.group10app.DB.LocalDB.WorkoutContract.WorkoutEntry;
 
-// TODO: read and see how to use this class
-public class LocalDB extends SQLiteOpenHelper {
-    public static final String USER_TABLE = "USER_TABLE";
+import java.util.ArrayList;
+import java.util.List;
 
-    public LocalDB(@Nullable Context context) {
-        super(context, "users.db", null, 1);
+public class LocalDB {
+    DbHelper exerciseDbHelper;
+    DbHelper workoutDbHelper;
+    DbHelper exerciseWorkoutPairDbHelper;
+
+    SQLiteDatabase db1;
+    SQLiteDatabase db2;
+    SQLiteDatabase db3;
+
+    public LocalDB(Context context) {
+        exerciseDbHelper = new DbHelper(context, "Exercise.db");
+        workoutDbHelper = new DbHelper(context, "Workout.db");
+        exerciseWorkoutPairDbHelper = new DbHelper(context, "ExerciseWorkoutPair.db");
+
+        // Gets the data repository in write mode
+        db1 = exerciseDbHelper.getWritableDatabase();
+        db2 = workoutDbHelper.getWritableDatabase();
+        db3 = exerciseWorkoutPairDbHelper.getWritableDatabase();
     }
 
-    // First time a DB is accessed. Should create a new table.
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE " + USER_TABLE + " (" +
-                "UserID INTEGER PRIMARY KEY," +
-                "Email TEXT NOT NULL," +
-                "FirstName TEXT NOT NULL," +
-                "SecondName TEXT NOT NULL," +
-                "Password TEXT NOT NULL," +
-                "DOB DATE NOT NULL," +
-                "Weight REAL," +
-                "Height REAL," +
-                "Sex TEXT," +
-                "Allergies TEXT," +
-                "ReasonForDownloading TEXT" +
-                ");";
-        db.execSQL(createTable);
+    public void insertExercise(String exerciseName, String description, String illustration, String targetMuscleGroup, String equipment, int difficulty) {
+        ContentValues values = new ContentValues();
+        values.put(ExerciseContract.ExerciseEntry.COLUMN_NAME_EXERCISE_NAME, exerciseName);
+        values.put(ExerciseContract.ExerciseEntry.COLUMN_NAME_DESCRIPTION, description);
+        values.put(ExerciseContract.ExerciseEntry.COLUMN_NAME_ILLUSTRATION, illustration);
+        values.put(ExerciseContract.ExerciseEntry.COLUMN_NAME_TARGET_MUSCLE_GROUP, targetMuscleGroup);
+        values.put(ExerciseContract.ExerciseEntry.COLUMN_NAME_EQUIPMENT, equipment);
+        values.put(ExerciseContract.ExerciseEntry.COLUMN_NAME_DIFFICULTY, difficulty);
+
+        long newRowId = db1.insert(ExerciseContract.ExerciseEntry.TABLE_NAME, null, values);
     }
 
-    // Called if DB version number changes. Prevents loss of information if the DB design changes.
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public List readExercise(String targetMuscleGroup) {
+        String[] projection = {
+                ExerciseContract.ExerciseEntry._ID,
+                ExerciseContract.ExerciseEntry.COLUMN_NAME_EXERCISE_NAME,
+                ExerciseContract.ExerciseEntry.COLUMN_NAME_DESCRIPTION,
+                ExerciseContract.ExerciseEntry.COLUMN_NAME_ILLUSTRATION,
+                ExerciseContract.ExerciseEntry.COLUMN_NAME_TARGET_MUSCLE_GROUP,
+                ExerciseContract.ExerciseEntry.COLUMN_NAME_EQUIPMENT,
+                ExerciseContract.ExerciseEntry.COLUMN_NAME_DIFFICULTY
+        };
 
+        String selection = ExerciseContract.ExerciseEntry.COLUMN_NAME_TARGET_MUSCLE_GROUP + " = ?";
+        String[] selectionArgs = {targetMuscleGroup};
+
+        String sortOrder = ExerciseContract.ExerciseEntry.COLUMN_NAME_EXERCISE_NAME + " DESC";
+
+        Cursor cursor = db1.query(
+                ExerciseContract.ExerciseEntry.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+        );
+
+        List itemIds = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            long itemId = cursor.getLong(cursor.getColumnIndexOrThrow(ExerciseContract.ExerciseEntry._ID));
+            itemIds.add(itemId);
+        }
+        cursor.close();
+
+        return itemIds;
     }
 
-    public boolean addUser(WorkoutModel workoutModel) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues cv = new ContentValues();
+    public void insertWorkout(int duration, String muscleGroup, String equipment, int difficulty) {
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(WorkoutEntry.COLUMN_NAME_DURATION, duration);
+        values.put(WorkoutEntry.COLUMN_NAME_MUSCLE_GROUP, muscleGroup);
+        values.put(WorkoutEntry.COLUMN_NAME_EQUIPMENT, equipment);
+        values.put(WorkoutEntry.COLUMN_NAME_DIFFICULTY, difficulty);
 
-//        cv.put("Email", workoutModel.getEmail());
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db2.insert(WorkoutEntry.TABLE_NAME, null, values);
+    }
 
-        return true;
+    public List readWorkout(String muscleGroup) {
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                WorkoutEntry._ID,
+                WorkoutEntry.COLUMN_NAME_DURATION,
+                WorkoutEntry.COLUMN_NAME_MUSCLE_GROUP,
+                WorkoutEntry.COLUMN_NAME_EQUIPMENT,
+                WorkoutEntry.COLUMN_NAME_DIFFICULTY
+        };
+
+        // Filter results WHERE "title" = 'My Title'
+        String selection = WorkoutEntry.COLUMN_NAME_MUSCLE_GROUP + " = ?";
+        String[] selectionArgs = {muscleGroup};
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                WorkoutEntry.COLUMN_NAME_DURATION + " DESC";
+
+        Cursor cursor = db2.query(
+                WorkoutEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+
+        List itemIds = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            long itemId = cursor.getLong(
+                    cursor.getColumnIndexOrThrow(WorkoutEntry._ID));
+            itemIds.add(itemId);
+        }
+        cursor.close();
+
+        return itemIds;
+    }
+
+    public void insertExerciseWorkoutPair(int exerciseID, int workoutID) {
+        ContentValues values = new ContentValues();
+        values.put(ExerciseWorkoutPairContract.ExerciseWorkoutPairEntry.COLUMN_NAME_EXERCISE_ID, exerciseID);
+        values.put(ExerciseWorkoutPairContract.ExerciseWorkoutPairEntry.COLUMN_NAME_WORKOUT_ID, workoutID);
+
+        long newRowId = db3.insert(ExerciseWorkoutPairContract.ExerciseWorkoutPairEntry.TABLE_NAME, null, values);
+    }
+
+    public List readExerciseWorkoutPair(int workoutID) {
+        String[] projection = {
+                ExerciseWorkoutPairContract.ExerciseWorkoutPairEntry._ID,
+                ExerciseWorkoutPairContract.ExerciseWorkoutPairEntry.COLUMN_NAME_EXERCISE_ID,
+                ExerciseWorkoutPairContract.ExerciseWorkoutPairEntry.COLUMN_NAME_WORKOUT_ID
+        };
+
+        String selection = ExerciseWorkoutPairContract.ExerciseWorkoutPairEntry.COLUMN_NAME_WORKOUT_ID + " = ?";
+        String[] selectionArgs = {String.valueOf(workoutID)};
+
+        String sortOrder = ExerciseWorkoutPairContract.ExerciseWorkoutPairEntry._ID + " DESC";
+
+        Cursor cursor = db3.query(
+                ExerciseWorkoutPairContract.ExerciseWorkoutPairEntry.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+        );
+
+        List itemIds = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            long itemId = cursor.getLong(cursor.getColumnIndexOrThrow(ExerciseWorkoutPairContract.ExerciseWorkoutPairEntry._ID));
+            itemIds.add(itemId);
+        }
+        cursor.close();
+
+        return itemIds;
+    }
+
+    public void insertSampleData() {
+        // Insert a sample row into the Exercise table
+        insertExercise("Sample Exercise", "This is a sample exercise.", "Sample illustration", "Sample muscle group", "Sample equipment", 1);
+
+        // Insert a sample row into the Workout table
+        insertWorkout(30, "Sample muscle group", "Sample equipment", 1);
+
+        // Get the IDs of the inserted Exercise and Workout
+        List exerciseIds = readExercise("Sample muscle group");
+        List workoutIds = readWorkout("Sample muscle group");
+
+        // Check if the IDs were retrieved successfully
+        if (!exerciseIds.isEmpty() && !workoutIds.isEmpty()) {
+            // Insert a sample row into the ExerciseWorkoutPair table
+            insertExerciseWorkoutPair((int) exerciseIds.get(0), (int) workoutIds.get(0));
+        }
+    }
+
+    public void printDataForDebugging() {
+        // Print data from Exercise table
+        List exerciseIds = readExercise("Sample muscle group");
+        for (Object id : exerciseIds) {
+            Log.d("Exercise Data", "ID: " + id);
+        }
+
+        // Print data from Workout table
+        List workoutIds = readWorkout("Sample muscle group");
+        for (Object id : workoutIds) {
+            Log.d("Workout Data", "ID: " + id);
+        }
+
+        // Print data from ExerciseWorkoutPair table
+        List exerciseWorkoutPairIds = readExerciseWorkoutPair((int) workoutIds.get(0));
+        for (Object id : exerciseWorkoutPairIds) {
+            Log.d("ExerciseWorkoutPair Data", "ID: " + id);
+        }
     }
 }
