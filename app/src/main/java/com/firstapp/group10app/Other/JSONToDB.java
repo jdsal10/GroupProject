65aacf777456803d0352294f71a518e027a0c580
TheRangerUnknown <et365@kent.ac.uk>
2024-02-19 13:32:09 +0000

    Added code to add ai workout

--- app/src/main/java/com/firstapp/group10app/Other/JSONToDB.java
+++ app/src/main/java/com/firstapp/group10app/Other/JSONToDB.java
@@ -29,4 +29,42 @@ public class JSONToDB {
             int eid = Integer.parseInt(e);
         linkExercise(id, eid);
     }}
-}
+
+
+    public static void insertWorkoutAI(JSONObject data) throws JSONException {
+        String[] workoutDetails = new String[5];
+
+        workoutDetails[0] = data.getString("WorkoutName");
+        workoutDetails[1] = data.getString("WorkoutDuration");
+        workoutDetails[2] = data.getString("TargetMuscleGroup");
+        workoutDetails[3] = data.getString("Equipment");
+        workoutDetails[4] = data.getString("Difficulty");
+
+        Integer id = DBHelper.insertWorkout(workoutDetails);
+
+        String exerciseList = data.getString("Exercises");
+
+        insertExercise(exerciseList, id);
+    }
+
+    public static void insertExercise(String data, Integer id) throws JSONException {
+        JSONArray exerciseArray = new JSONArray(data);
+        JSONObject individualExercise;
+        String[] exerciseData = new String[5];
+
+        for (int i = 0; i < exerciseArray.length(); i++) {
+            individualExercise = exerciseArray.getJSONObject(i);
+            exerciseData[0] = individualExercise.getString("ExerciseName");
+            exerciseData[1] = individualExercise.getString("Description");
+            exerciseData[2] = individualExercise.getString("TargetMuscleGroup");
+            exerciseData[3] = individualExercise.getString("Equipment");
+            exerciseData[4] = individualExercise.getString("Difficulty");
+
+            DBHelper.insertExercise(exerciseData, id);
+            System.out.println(Arrays.toString(exerciseData));
+        }
+    }
+      }
+
+
+
