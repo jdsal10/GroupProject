673ffd8efb2f1a6b0a293e5e7f0cb8490a925f1a
TheRangerUnknown <127422940+TheRangerUnknown@users.noreply.github.com>
2024-02-19 18:59:32 +0000

    Removed code
    Related method has drastically changed

--- app/src/main/java/com/firstapp/group10app/Pages/MainActivity.java
+++ app/src/main/java/com/firstapp/group10app/Pages/MainActivity.java
@@ -2,23 +2,16 @@ package com.firstapp.group10app.Pages;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firstapp.group10app.DB.DBConnection;
-import com.firstapp.group10app.Other.JSONToDB;
 import com.firstapp.group10app.Other.Session;
 import com.firstapp.group10app.R;
-
-import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -78,47 +71,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             overridePendingTransition(R.anim.slide_left_in, R.anim.slide_right_out);
         }
     }
-
-//    public void addData() throws JSONException {
-//        String data = "{" +
-//                "\"WorkoutName\": \"Abs Blast\"," +
-//                "\"WorkoutDuration\": 35," +
-//                "\"TargetMuscleGroup\": \"Abs\"," +
-//                "\"Equipment\": \"None\"," +
-//                "\"Difficulty\": \"Easy\"," +
-//                "\"exercises\": [" +
-//                "{" +
-//                "\"ExerciseName\": \"Crunches\"," +
-//                "\"Description\": \"Lie on your back with knees bent and hands behind your head. Lift your shoulders off the floor using your abdominal muscles, then lower back down.\"," +
-//                "\"TargetMuscleGroup\": \"Abs\"," +
-//                "\"Equipment\": \"None\"," +
-//                "\"Difficulty\": \"Easy\"" +
-//                "}," +
-//                "{" +
-//                "\"ExerciseName\": \"Leg Raises\"," +
-//                "\"Description\": \"Lie flat on your back and lift your legs upward, keeping them straight until they form a 90-degree angle with the floor, then lower them back down slowly.\"," +
-//                "\"TargetMuscleGroup\": \"Lower Abs\"," +
-//                "\"Equipment\": \"None\"," +
-//                "\"Difficulty\": \"Easy\"" +
-//                "}," +
-//                "{" +
-//                "\"ExerciseName\": \"Bicycle Crunches\"," +
-//                "\"Description\": \"Lie on your back with knees bent and hands behind your head. Bring one knee towards your chest while simultaneously twisting through your core to bring the opposite elbow towards the knee. Alternate sides in a pedaling motion.\"," +
-//                "\"TargetMuscleGroup\": \"Obliques\"," +
-//                "\"Equipment\": \"None\"," +
-//                "\"Difficulty\": \"Easy\"" +
-//                "}," +
-//                "{" +
-//                "\"ExerciseName\": \"Plank\"," +
-//                "\"Description\": \"Assume a push-up position, but with your weight on your forearms instead of your hands. Keep your body in a straight line from head to heels, engaging your core muscles.\"," +
-//                "\"TargetMuscleGroup\": \"Core\"," +
-//                "\"Equipment\": \"None\"," +
-//                "\"Difficulty\": \"Easy\"" +
-//                "}" +
-//                "]" +
-//                "}";
-//
-//        JSONToDB.insertWorkout(data);
-//
-//    }
 }
